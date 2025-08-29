f7edce3fdf7545fcbdd61d263e26154e6dd74e65
Savitri353 <savitrigamit123456@gmail.com>
2025-08-29 16:39:30 +0530

    Time: 6 ms (42.44%) | Memory: 43.3 MB (70.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,17 +5,16 @@ class Solution {
        }
 
        int[] count1 = new int[26];
-       int[] count2 = new int[26];
-
+    
        for(int i=0; i<s.length(); i++) {
             char s1 = s.charAt(i);
             char t2 = t.charAt(i);
             count1[s1-'a']++;
-            count2[t2-'a']++;
+            count1[t2-'a']--;
        }
 
-      for(int i=0; i<count1.length; i++) {
-        if(count1[i] != count2[i]) {
+      for(int c:count1) {
+        if(c != 0) {
             return false;
         }
       } 
