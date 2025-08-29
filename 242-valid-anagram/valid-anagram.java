2701f21ad0c888f6411c7affe775c0f3cfcff48e
Savitri353 <savitrigamit123456@gmail.com>
2025-08-29 18:46:23 +0530

    Time: 14 ms (27.84%) | Memory: 45.1 MB (6.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,20 +4,42 @@ class Solution {
           return false;
        }
 
-       int[] count1 = new int[26];
-    
-       for(int i=0; i<s.length(); i++) {
-            char s1 = s.charAt(i);
-            char t2 = t.charAt(i);
-            count1[s1-'a']++;
-            count1[t2-'a']--;
-       }
+        HashMap<Character, Integer> map = new HashMap<>();
 
-      for(int c:count1) {
-        if(c != 0) {
-            return false;
+        //count frequency of character in s
+        for(char c:s.toCharArray()) {
+            map.put(c, map.getOrDefault(c,0)+1);
         }
-      } 
-        return true;
+
+        //now decrease frequency of character using character of t
+        for(char c:t.toCharArray()) {
+            if(!map.containsKey(c)) {
+                return false;
+            }
+
+            map.put(c, map.get(c)-1);
+            if(map.get(c)==0) {
+                map.remove(c);
+            }
+        }
+
+        return map.isEmpty();
+
+
+    //    int[] count1 = new int[26];
+    
+    //    for(int i=0; i<s.length(); i++) {
+    //         char s1 = s.charAt(i);
+    //         char t2 = t.charAt(i);
+    //         count1[s1-'a']++;
+    //         count1[t2-'a']--;
+    //    }
+
+    //   for(int c:count1) {
+    //     if(c != 0) {
+    //         return false;
+    //     }
+    //   } 
+    //     return true;
     }
 }
