b6269d3248a2f9a8ce3a1387197c81a00a760126
Savitri353 <savitrigamit123456@gmail.com>
2025-08-29 19:28:57 +0530

    Time: 4 ms (56.45%) | Memory: 43.5 MB (55.76%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,48 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder("");
 
-            int i=0, j=s.length()-1;
-            while(i<j) {
-                while(i<j && !Character.isLetterOrDigit(s.charAt(i)))i++;
-                while(i<j && !Character.isLetterOrDigit(s.charAt(j)))j--;  
+        for(char c:s.toCharArray()) {
+            if(Character.isLetterOrDigit(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
 
-                if(i<j && (Character.toLowerCase(s.charAt(i))) != (Character.toLowerCase(s.charAt(j)))) return false;
-                i++;
-                j--;
+        int i = 0;
+        int j=sb.length()-1;
+        while(i<=j) {
+            if(sb.charAt(i) != sb.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
 
-                }
-
-            return true;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // int i=0, j= s.length()-1;
-
-        // while(i<j) {
-        //     while(i<j && !Character.isLetterOrDigit(s.charAt(i)))i++;
-        //     while(j>i  && !Character.isLetterOrDigit(s.charAt(j)))j--;
-
-        //     if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
-
-        //     i++;
-        //     j--;
-        // }
-
-        // return true;
+        return true;
     }
 }
