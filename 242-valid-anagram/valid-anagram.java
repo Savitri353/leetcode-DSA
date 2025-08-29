4633a0d5f9661dce66ff1f9bcf0e082757f65d48
Savitri353 <savitrigamit123456@gmail.com>
2025-08-29 18:46:55 +0530

    Time: 15 ms (25.05%) | Memory: 44.9 MB (16.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -25,21 +25,5 @@ class Solution {
 
         return map.isEmpty();
 
-
-    //    int[] count1 = new int[26];
-    
-    //    for(int i=0; i<s.length(); i++) {
-    //         char s1 = s.charAt(i);
-    //         char t2 = t.charAt(i);
-    //         count1[s1-'a']++;
-    //         count1[t2-'a']--;
-    //    }
-
-    //   for(int c:count1) {
-    //     if(c != 0) {
-    //         return false;
-    //     }
-    //   } 
-    //     return true;
     }
 }
