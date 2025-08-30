2961b72ae84a552fe7c7a2ed98c2bdbfb61971bf
Savitri353 <savitrigamit123456@gmail.com>
2025-08-30 17:32:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (62.44%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] v) {
+        if(v.length==0 || v==null) {
+            return "";
+        }
+       Arrays.sort(v);
+        String first = v[0];
+        String last = v[v.length-1];
+        int i=0;
+       while(i<first.length() && i<last.length() && first.charAt(i)==last.charAt(i)) {
+            i++;
+       }
+
+       return first.substring(0,i);
+    }
+}
