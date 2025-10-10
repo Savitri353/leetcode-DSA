6cf78bef51031ff84bf60dd729d012a3d8f5d3c3
Savitri353 <savitrigamit123456@gmail.com>
2025-10-10 18:58:43 +0530

    Time: 24 ms (32.86%) | Memory: 50.6 MB (87.85%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -11,11 +11,7 @@ class Solution {
                 } else if(text1.charAt(i-1) == text2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1]+1;
                 } else {
-                    if(dp[i-1][j] > dp[i][j-1]) {
-                        dp[i][j] = dp[i-1][j];
-                    } else {
-                        dp[i][j]=dp[i][j-1];
-                    }
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
