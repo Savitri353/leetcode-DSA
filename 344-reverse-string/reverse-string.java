b70d805fa902f0c71b5dd99090b1803f4d0e8cf1
Savitri353 <savitrigamit123456@gmail.com>
2025-12-08 22:15:09 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (61.02%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,8 +1,5 @@
 class Solution {
     public void reverseString(char[] s) {
-        if(s.length==0) {
-            return;
-        }
         int i=0, j=s.length-1;
         while(i<j) {
             char temp = s[i];
