802cedda087d5db1935cd8ac54da1ef9c1370e57
Savitri353 <savitrigamit123456@gmail.com>
2025-12-16 18:26:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (52.36%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -15,8 +15,8 @@ class Solution {
 
         while(list1 != null && list2 != null) {
             if(list1.val<=list2.val) {
-               tail.next = list1;
-               list1 = list1.next;
+                tail.next=list1;
+                list1 = list1.next;
             } else {
                 tail.next = list2;
                 list2 = list2.next;
@@ -25,7 +25,7 @@ class Solution {
             tail = tail.next;
         }
 
-    //attach remaining nodes
+
         if(list1 != null) {
             tail.next = list1;
         } else {
