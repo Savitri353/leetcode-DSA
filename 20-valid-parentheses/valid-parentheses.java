617f06badf4c7b08722f96ba493fa57c4c40af1d
Savitri353 <savitrigamit123456@gmail.com>
2025-12-18 21:27:25 +0530

    Time: 5 ms (16.11%) | Memory: 43.4 MB (17.53%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,23 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
+        if(s.length()<2) {
+            return false;
+        }
 
+        Stack<Character> st = new Stack<>();
         for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i)=='(' ||s.charAt(i)=='{' ||s.charAt(i)=='[') {
+
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                 st.push(s.charAt(i));
-            } else if(s.charAt(i)==')' && !st.isEmpty() && st.peek()=='('){
+            } else if(s.charAt(i) == ')' && !st.empty() && st.peek() == '(') {
                 st.pop();
-            } else if(s.charAt(i)=='}' && !st.isEmpty() && st.peek()=='{'){
+            } else if(s.charAt(i) == '}' && !st.empty() && st.peek() == '{') {
                 st.pop();
-            } else if(s.charAt(i)==']' && !st.isEmpty() && st.peek() == '[') {
+            } else if(s.charAt(i) == ']' && !st.empty() && st.peek() == '[') {
                 st.pop();
             } else {
                 return false;
             }
-
+            
         }
 
-        return st.isEmpty();
-    }
+        return st.empty();
+    }   
 }    
       
