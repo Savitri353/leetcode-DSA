1778f092fa7d5214ae7c141643c033a637185eeb
Savitri353 <savitrigamit123456@gmail.com>
2025-12-20 14:49:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (10.15%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,25 +1,25 @@
 class MyStack {
 
-    Queue<Integer> q1;
-    Queue<Integer> q2;
+   Queue<Integer> q1;
+   Queue<Integer> q2;
     public MyStack() {
-      q1=new LinkedList<>();
-       q2=new LinkedList<>();
+      q1 = new LinkedList<>();
+      q2 = new LinkedList<>();
     }
     
     public void push(int x) {
-       while(!q1.isEmpty()) {
+        while(!q1.isEmpty()) {
             q2.add(q1.remove());
-       }
+        }
 
-       q1.add(x);
-       while(!q2.isEmpty()) {
-        q1.add(q2.remove());
-       }
+        q1.add(x);
+        while(!q2.isEmpty()) {
+            q1.add(q2.remove());
+        }
     }
     
     public int pop() {
-       return q1.remove(); 
+       return q1.remove();
     }
     
     public int top() {
