0add267b5b5d07137a395881dc00f48681e3aea3
Savitri353 <savitrigamit123456@gmail.com>
2026-01-10 10:48:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (5.94%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,9 +15,42 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-       if(p==null && q==null) return true;
-       if(p==null || q== null) return false;
-       if(p.val!=q.val) return false;
-       return isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
+        //approach 1
+    //    if(p==null && q==null) return true;
+    //    if(p==null || q== null) return false;
+    //    if(p.val!=q.val) return false;
+    //    return isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
+
+    //approach 2
+            if(p==null && q==null) return true;
+            if(p==null || q==null) return false;
+            Queue<TreeNode> q1 = new LinkedList<>();
+            Queue<TreeNode> q2 = new LinkedList<>();
+            q1.add(p);
+            q2.add(q);
+            while(!q1.isEmpty() && !q2.isEmpty()) {
+                TreeNode node1 = q1.poll();
+                TreeNode node2 = q2.poll();
+
+                if(node1.val != node2.val) {
+                    return false;
+                }
+
+                if(node1.left != null && node2.left != null) {
+                    q1.add(node1.left);
+                    q2.add(node2.left);
+                } else if(node1.left != null || node2.left != null) {
+                    return false;
+                }
+
+                if(node1.right != null && node2.right != null) {
+                    q1.add(node1.right);
+                    q2.add(node2.right);
+                } else if(node1.right != null || node2.right != null) {
+                    return false;
+                }
+            }
+
+            return true;
     }
 }    
