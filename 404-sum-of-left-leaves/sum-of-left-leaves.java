62d005c383d330d8e6658bce5bb75c874a64449b
Savitri353 <savitrigamit123456@gmail.com>
2026-01-17 19:37:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (6.77%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -14,24 +14,40 @@
  * }
  */
 class Solution {
-    public int sumOfLeftLeaves(TreeNode root) {
 
-     return solve(root, null); 
-    }
+     public int sumOfLeftLeaves(TreeNode root) {
+        return solve(root, false);
+     }
 
-    public int solve(TreeNode current, TreeNode parent) {
+     public int solve(TreeNode current, boolean isLeft) {
         if(current == null) return 0;
 
-        //leaf
-        if(current.left == null && current.right==null) {
-            if(parent != null && parent.left == current) {
+        if(current.left == null && current.right == null) {
+            if(isLeft) {
                 return current.val;
             }
         }
 
-        int left = solve(current.left, current); 
-        int right = solve(current.right, current); 
+        return solve(current.left, true) + solve(current.right, false);
+     }
+    // public int sumOfLeftLeaves(TreeNode root) {
 
-        return left+right;
-    }
+    //  return solve(root, null); 
+    // }
+
+    // public int solve(TreeNode current, TreeNode parent) {
+    //     if(current == null) return 0;
+
+    //     //leaf
+    //     if(current.left == null && current.right==null) {
+    //         if(parent != null && parent.left == current) {
+    //             return current.val;
+    //         }
+    //     }
+
+    //     int left = solve(current.left, current); 
+    //     int right = solve(current.right, current); 
+
+    //     return left+right;
+    // }
 }
