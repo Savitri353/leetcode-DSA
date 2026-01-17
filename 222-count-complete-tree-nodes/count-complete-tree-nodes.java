e169a0e74496666093a68cd4c17623abb4a2ce8b
Savitri353 <savitrigamit123456@gmail.com>
2026-01-17 14:56:06 +0530

    Time: 0 ms (100.00%) | Memory: 49.7 MB (8.04%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,18 +14,31 @@
  * }
  */
 class Solution {
-    int count = 0;
-    public int countNodes(TreeNode root) {
-        if(root == null) return 0;
-        solve(root);
-        return count;
-    }
+     public int countNodes(TreeNode root) {
+        if(root==null) return 0;
 
-    public void solve(TreeNode root) {
-        if(root == null) return;
+        int leftN = countNodes(root.left);
+        int rightN = countNodes(root.right);
 
-        solve(root.left);
-        count++;
-        solve(root.right);
-    }
+        return leftN +rightN+1;
+     }
 }
+
+
+    //Each recursive call returns the number of nodes in its subtree, and the parent combines those values.
+
+
+    // int count = 0;
+    // public int countNodes(TreeNode root) {
+    //     if(root == null) return 0;
+    //     solve(root);
+    //     return count;
+    // }
+
+    // public void solve(TreeNode root) {
+    //     if(root == null) return;
+
+    //     solve(root.left);
+    //     count++;
+    //     solve(root.right);
+    // }
