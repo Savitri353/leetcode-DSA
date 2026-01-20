fb9283521ab5719055ebc2ccc58dd139c7d09184
Savitri353 <savitrigamit123456@gmail.com>
2026-01-20 16:26:08 +0530

    Time: 35 ms (44.12%) | Memory: 59.1 MB (79.43%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,40 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if(nums == null || nums.length<3) return new ArrayList<>();
+        List<List<Integer>> list  = new ArrayList<>();
         Arrays.sort(nums);
 
-        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0; i<=nums.length-3; i++) {
+            if(i>0 && nums[i] == nums[i-1]) continue;
 
-        for(int i=0; i<nums.length-2; i++) {
-            //dealing with duplicate elements
-           if(i>0 && nums[i] == nums[i-1]) {
-            continue;
-           }
+            int left = i+1;
+            int right = nums.length-1;
 
-           int left = i+1;
-           int right = nums.length-1;
-
-
-           while(left < right) {
+            while(left<right) {
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if(sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    //skip duplicate 'left' and 'right'
                     while(left<right && nums[left] == nums[left+1]) left++;
                     while(left<right && nums[right] == nums[right-1]) right--;
 
                     left++;
                     right--;
-                } else if(sum<0) {
+                } else if( sum<0) {
                     left++;
                 } else {
                     right--;
                 }
-           }
+            } 
         }
 
-        return result;
+        return list;
     }
 }
+
+//sorting
+//duplicate avoide
+//fixed one, and duplicate avoidance
