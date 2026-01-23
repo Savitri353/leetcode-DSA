c8aad97906bcdad0d91cd7cdf0ae17d1b7ef4428
Savitri353 <savitrigamit123456@gmail.com>
2026-01-23 09:19:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (96.47%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,47 +1,57 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
-        if(matrix.length==0) {
-            return result;
-        }
+       List<Integer> list = new ArrayList<>();
 
-        int bottom=matrix.length-1; //row
-        int right=matrix[0].length-1; //col
-        int left = 0;
-        int top=0;
+       int m=matrix.length-1; //row
+       int n=matrix[0].length-1;//col
 
-       while(bottom>=top && right>=left) {
-         
-         //traverse horizontal, from left to right, col++
-         for(int i=left; i<=right; i++) {
-            result.add(matrix[top][i]);
-         }
-         top++;
+       int top=0;
+       int right=n;
+       int bottom=m;
+       int left=0;
 
-         //traverse vertical, from top to bottom
-         for(int i=top; i<=bottom; i++) {
-            result.add(matrix[i][right]);
-         }
-        right--;
+       while(top<=bottom && left<=right) {
 
-         //traverse bottom row, right to left
-         if(top<=bottom) {
-          for(int i=right; i>=left; i--) {
-            result.add(matrix[bottom][i]);
-          }
-          bottom--;
-         }
-            
-
-         //traverse left col
-         if(left<=right) {
-            for(int i=bottom; i>=top; i--) {
-                result.add(matrix[i][left]);
+            //top, printing top, top is constant(row)
+            for(int i=left; i<=right; i++) {
+                list.add(matrix[top][i]);
             }
-            left++;
-         }
+            //update top , as first row is already printed , now it's next row's turn will be there
+            //shrink the matrix
+            top++;
+
+            //print right side when col is constant
+            for(int i=top; i<=bottom; i++) {
+                list.add(matrix[i][right]);
+            }
+
+            //shrink from right
+            right--;
+
+            //print from bottom
+            if(top<=bottom) {
+                
+                for(int i=right; i>=left; i--) {
+                    list.add(matrix[bottom][i]);
+                }
+                 //shrink from bottom
+                 bottom--;
+            }
+
+            //print left side
+            if(right>=left) {
+                for(int i=bottom; i>=top; i--) {
+                    list.add(matrix[i][left]);
+                }
+                 //shrink from left
+                left++;
+            }
+
+           
+
+            //now first time you printed, now again go in shrinked matrix.
        }
 
-       return result;
+       return list;
     }
 }
