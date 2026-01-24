dbec0718366c06da14bdfa0b4c1dad3e4e995eb8
Savitri353 <savitrigamit123456@gmail.com>
2026-01-24 16:28:09 +0530

    Time: 1 ms (98.02%) | Memory: 47.5 MB (30.25%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,26 +1,45 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        for(int i=0; i<nums.length; i++) {
-            int left = 0;
-            int right = 0;
-            int j=i;
-            
-            int k=i;
-            while(j>=0) {
-                left += nums[j]; 
-                j--; 
-            }
-            left-=nums[i];
-            while(k<=nums.length-1) {
-                right+=nums[k];
-                k++;
-            }
+        
+        int n=nums.length;
+        int totalSum = 0;
+        //find total sum
+        for(int i=0; i<n; i++) {
+            totalSum+=nums[i];
+        }
 
-            right-=nums[i];
-
-            if(left == right) return i;
-        }  
+        int leftSum=0;
+        int rightSum=0;
+        for(int i=0; i<n; i++) {
+           rightSum=totalSum-leftSum-nums[i];
+           if(leftSum == rightSum) return i;
+           leftSum+=nums[i];
+           
+        }
 
         return -1;
     }
 }
+
+    // for(int i=0; i<nums.length; i++) {
+    //         int left = 0;
+    //         int right = 0;
+    //         int j=i;
+            
+    //         int k=i;
+    //         while(j>=0) {
+    //             left += nums[j]; 
+    //             j--; 
+    //         }
+    //         left-=nums[i];
+    //         while(k<=nums.length-1) {
+    //             right+=nums[k];
+    //             k++;
+    //         }
+
+    //         right-=nums[i];
+
+    //         if(left == right) return i;
+    //     }  
+
+    //     return -1;
