dadb52927ab5d7b114700bd9859f983227f4bd93
Savitri353 <savitrigamit123456@gmail.com>
2026-01-25 22:04:24 +0530

    Time: 4 ms (79.72%) | Memory: 44.6 MB (60.55%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,21 +4,21 @@ class Solution {
             return false;
         }
 
-        int[] arr = new int[26];
+        int[] freq = new int[26];
+
         for(int i=0; i<s.length(); i++) {
-            arr[s.charAt(i)-'a']++;
+            freq[s.charAt(i)-'a']++;
         }
 
-       for(int i=0; i<t.length(); i++) {
-            arr[t.charAt(i)-'a']--;
+        for(int i=0; i<t.length(); i++) {
+            freq[t.charAt(i)-'a']--;
         }
 
-        for(int i=0; i<arr.length; i++) {
-           if(arr[i]!=0) {
-            return false;
-           }
+        for(int i=0; i<freq.length; i++) {
+            if(freq[i] != 0) return false;
         }
 
         return true;
     }
 }
+
