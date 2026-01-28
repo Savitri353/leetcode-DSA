f157340ed4cfec46694cd6489dfedf4fc468a467
Savitri353 <savitrigamit123456@gmail.com>
2026-01-28 09:53:50 +0530

    Time: 4 ms (65.03%) | Memory: 82.3 MB (7.83%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -16,20 +16,19 @@
 class Solution {
    
     public int minDepth(TreeNode root) {
-        if(root==null) return 0;
+       if(root == null) return 0;
 
-        //if left is null go right
-        if(root.left == null) {
+       //if left is null go towards right,
+       //n1-n2-n3-n4,, to avoid this kind of tree that is like chain there we can't return 1
+
+       if(root.left == null) {
             return 1+minDepth(root.right);
-        }
-        //if right is null go left
-        if(root.right == null) {
-            return 1+minDepth(root.left);
-        }
+       } 
 
-        int left = minDepth(root.left);
-        int right = minDepth(root.right);
-        
-        return 1+ Math.min(left, right);
+       if(root.right == null) {
+            return 1+minDepth(root.left);
+       }
+
+       return 1 + Math.min( minDepth(root.left), minDepth(root.right) );
     }
 }
