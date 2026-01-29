544c2be697b8a92606c2943c86775bf2639e0372
Savitri353 <savitrigamit123456@gmail.com>
2026-01-29 18:27:19 +0530

    Time: 6 ms (9.63%) | Memory: 46.9 MB (26.55%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,25 +1,25 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> mp = new HashMap<>();
+      HashMap<Character, Integer> mp = new HashMap<>();
 
-        mp.put('I', 1);
-        mp.put('V', 5);
-        mp.put('X', 10);
-        mp.put('L', 50);
-        mp.put('C', 100);
-        mp.put('D', 500);
-        mp.put('M', 1000);
+      mp.put('I', 1);
+      mp.put('V', 5);
+      mp.put('X', 10);
+      mp.put('L', 50);
+      mp.put('C', 100);
+      mp.put('D', 500);
+      mp.put('M', 1000);
+     //roman string should be in decreasing order from left to right
+      int result = 0;
+      for(int i=0; i<=s.length()-2; i++) {
+         if(mp.get(s.charAt(i)) < mp.get(s.charAt(i+1))) {
+            result-=mp.get(s.charAt(i));
+         } else {
+            result+=mp.get(s.charAt(i));
+         }
+      }
 
-        int result = 0;
-        for(int i=0; i<s.length()-1; i++) {
-            if(mp.get(s.charAt(i))<mp.get(s.charAt(i+1))) {
-                result-=mp.get(s.charAt(i));
-            } else {
-                result+=mp.get(s.charAt(i));
-            }
-        }
-
-        result+=mp.get(s.charAt(s.length()-1));
-        return result;
+      result+=mp.get(s.charAt(s.length()-1));
+      return result;
     }
 }
