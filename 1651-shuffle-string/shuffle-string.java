baae1814e29bf3cb7799d28ee41f91fd46a7fabf
Savitri353 <savitrigamit123456@gmail.com>
2026-02-04 21:52:01 +0530

    Time: 1 ms (61.36%) | Memory: 45.6 MB (70.22%) - LeetSync

--- 1651-shuffle-string/shuffle-string.java
+++ 1651-shuffle-string/shuffle-string.java
@@ -1,17 +1,27 @@
 class Solution {
     public String restoreString(String s, int[] ind) {
-        HashMap<Integer, Character> mp = new HashMap<>();
 
-        for(int i=0; i<ind.length; i++) {
-            mp.put(ind[i], s.charAt(i));
-        }
+        
+         char[] ans = new char[ind.length];
+         for(int i=0; i<ind.length; i++) {
+            ans[ind[i]] = s.charAt(i); //finding correct index in ans array 
+         }
+
+
+         return new String(ans);
+    //     //map original string's character to original ind elements
+    //     HashMap<Integer, Character> mp = new HashMap<>();
+
+    //     for(int i=0; i<ind.length; i++) {
+    //         mp.put(ind[i], s.charAt(i));
+    //     }
   
-        char[] ans = new char[ind.length];
-        for(int i=0; i<ind.length; i++) {
+    //     char[] ans = new char[ind.length];
+    //     for(int i=0; i<ind.length; i++) {
 
-            ans[ind[i]] = mp.get(ind[i]); //find the correct index in ans array and put the correct maped character to that int.
-        }
+    //         ans[ind[i]] = mp.get(ind[i]); //find the correct index in ans array and put the correct maped character to that int.
+    //     }
 
-       return new String(ans);
+    //    return new String(ans);
     }
 }
