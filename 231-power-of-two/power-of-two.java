64d25a1316e6e9c152b4505344a5752169052c96
Savitri353 <savitrigamit123456@gmail.com>
2026-03-04 08:00:28 +0530

    Time: 1 ms (96.13%) | Memory: 42.8 MB (35.24%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,12 +1,26 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-       if(n<=0) {
-        return false;
-       }
+    //     // Base case: negative numbers and 0 are not power of two
+    //    if(n<=0) {
+    //     return false;
+    //    }
 
-       while(n%2 == 0) {
-            n/=2;
-       }
-       return n==1;
+    //    // Base case: 1 is power of two (2^0)
+    //    if(n==1) {
+    //     return true;
+    //    }
+       
+    //    // If n is odd, it cannot be power of two
+    //    if(n%2 != 0) {
+    //     return false;
+    //    } else
+
+    //     // Recursive call
+    //    return isPowerOfTwo(n/2);
+
+    //without usign recursion
+
+        // Power of two numbers always have one in binary
+        return n>0 && (n & (n-1)) == 0;
      }
 }
