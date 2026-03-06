992d9175d1614215df0140e92766a7578b74dd1d
Savitri353 <savitrigamit123456@gmail.com>
2026-03-06 21:56:41 +0530

    Time: 5 ms (81.86%) | Memory: 77.4 MB (47.27%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,27 +1,47 @@
 class Solution {
     public int maxArea(int[] height) {
-        //idea over here, that is as width decreasing be greedy to find..
-        //max heigh by avoiding small height.
+    //     //idea over here, that is as width decreasing be greedy to find..
+    //     //max height by avoiding small height.
        
-       //1.width 2.height 3.maxArea 4. decrese from which side
-        int n = height.length;
-          //by two pointers
-        int i=0, j=n-1;
+    //    //1.width 2.height 3.maxArea 4. decrese from which side
+    //   int n=height.length;
+    //   //two pointers
+    //   int i=0, j=n-1;
+    //   int maxA = 0;
 
-        int maxA = 0;
-        
-       while(i<j) {
-            int width = j-i;
-            int minHeight = Math.min(height[i], height[j]);
-            int A = width*minHeight;
-            maxA = Math.max(maxA, A);
-            if(height[i]<height[j]) {
-                i++;
-            } else {
-                j--;
-            }
-       }
+    //   while(i<j) {
+    //     int width = j-i;
+    //     int minHeight = Math.min(height[i], height[j]);
+    //     int area = width*minHeight;
+    //     maxA = Math.max(maxA, area);
 
-       return maxA;
+    //     if(height[i]<height[j]) { //If you move left: Width decreases, 
+    //         i++;                  // BUT the height might increase
+    //     } else {
+    //         j--;
+    //     }
+    //   }
+
+    //   return maxA;
+    // }
+
+    int j=height.length-1;
+    int i=0;
+    int maxArea = 0;
+    while(i<j) {
+        int width = j-i;
+        int Minheight = Math.min(height[i], height[j]);
+        maxArea = Math.max(maxArea, width*Minheight);
+
+        if(height[i]<height[j]) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+
+    return maxArea;
     }
 }
+
+//since the water is limited by the shorter line, i use two pointers starting from both ends. At each step, i calculated the area and move the pointer with smaller height, becasue moving the taller one cant increase the area.this gives O(n) solution.
