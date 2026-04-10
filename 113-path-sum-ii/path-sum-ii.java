640bcf07a6510990e4ceda07b1950bf6cade16e9
Savitri353 <savitrigamit123456@gmail.com>
2026-04-10 15:31:56 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (89.33%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -15,26 +15,22 @@
  */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> list = new ArrayList<>();
-        solve(list, new ArrayList<>(), root, targetSum);
+       List<List<Integer>> list = new ArrayList<>();
+       solve(list, new ArrayList<>(), root, targetSum);
         return list;
     }
 
     public void solve(List<List<Integer>> list, List<Integer> temp, TreeNode root, int targetSum) {
-
         if(root == null) return;
-
         temp.add(root.val);
 
-        if(root.left==null && root.right==null && targetSum == root.val) {
-            list.add(new ArrayList<>(temp)); 
+        if(root.left == null && root.right == null && root.val == targetSum) {
+            list.add(new ArrayList<>(temp));
         }
 
         solve(list, temp, root.left, targetSum-root.val);
         solve(list, temp, root.right, targetSum-root.val);
 
-         //after exploring one valid path i need to bracktrack
-           temp.remove(temp.size()-1);
-        
+        temp.remove(temp.size()-1);
     }
 }
