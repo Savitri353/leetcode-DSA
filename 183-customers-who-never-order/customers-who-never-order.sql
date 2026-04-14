b5ae8edbced725a65c962a5cbe69f722074d19af
Savitri353 <savitrigamit123456@gmail.com>
2026-04-14 19:02:38 +0530

    Time: 560 ms (35.03%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,2 +1,2 @@
-select c1.name as Customers from Customers as c1 left join Orders as o1 on c1.id = o1.customerId 
+select c1.name Customers from Customers as c1 left join Orders o1 on c1.id = o1.customerId 
 where o1.id is null;
