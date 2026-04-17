c0781e1b42da7d8293f0afe2066b8272a92d6330
Savitri353 <savitrigamit123456@gmail.com>
2026-04-17 08:52:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (98.85%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,35 +1,36 @@
 class MyQueue {
-    Stack<Integer> s1;
-    Stack<Integer> s2;
+    Stack<Integer> st1;
+    Stack<Integer> st2;
+
     public MyQueue() {
-       s1 = new Stack<>();
-       s2 = new Stack<>();
+        st1 = new Stack<>();
+        st2 = new Stack<>();
     }
-    
-    public void push(int x) {
-       while(!s1.isEmpty()) {
-            s2.push(s1.pop());
-       }
 
-       s1.push(x);
+    public void push(int val) {
+        while(!st1.isEmpty()) {
+            st2.push(st1.pop());
+        }
 
-       while(!s2.isEmpty()) {
-            s1.push(s2.pop());
-       }
+        st1.push(val);
+
+        while(!st2.isEmpty()) {
+            st1.push(st2.pop());
+        }
     }
-    
+
     public int pop() {
-        return s1.pop();
+        return st1.pop();
     }
-    
+
     public int peek() {
-        return s1.peek();
+        return st1.peek();
     }
-    
+
     public boolean empty() {
-       return s1.isEmpty();
+        return st1.empty();
     }
-}
+}   
 
 /**
  * Your MyQueue object will be instantiated and called as such:
