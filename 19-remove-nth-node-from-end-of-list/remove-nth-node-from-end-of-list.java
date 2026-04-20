3796bdc34d620d823564cff6c21645cd95679220
Savitri353 <savitrigamit123456@gmail.com>
2026-04-20 21:52:15 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (42.98%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,33 +10,59 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-       //if there is only one node
-       if(head.next == null) {
-            return null;
-       }
+    //    //if there is only one node
+    //    if(head.next == null) {
+    //         return null;
+    //    }
 
-        //calculate size
-        ListNode temp = head;
-        int size=0;
-        while(temp != null) {
-            temp = temp.next;
-            size++;
-        }
+    //     //calculate size
+    //     ListNode temp = head;
+    //     int size=0;
+    //     while(temp != null) {
+    //         temp = temp.next;
+    //         size++;
+    //     }
 
-        if(n == size) {
-            return head.next;
-        }
-        //now remove the nth element from the last
-        ListNode prev = head;
-        int prevIndex = size-n;
-        int i=1;
-        while(i < prevIndex) {
-            prev = prev.next;
-            i++;
-        }
+    //     if(n == size) {
+    //         return head.next;
+    //     }
+    //     //now remove the nth element from the last
+    //     ListNode prev = head;
+    //     int prevIndex = size-n;
+    //     int i=1;
+    //     while(i < prevIndex) {
+    //         prev = prev.next;
+    //         i++;
+    //     }
 
-        prev.next = prev.next.next;
+    //     prev.next = prev.next.next;
 
-        return head;
+    //     return head;
+
+    // *********************
+    //two pointers
+
+    if(head.next == null) return null;
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+
+
+    ListNode fast = dummy;
+    ListNode slow = dummy;
+
+    
+    //move fast n+1 steps ahead
+    for(int i=1; i<=n+1; i++) {
+        fast = fast.next;
+       
+    }
+
+    while(fast != null) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    slow.next = slow.next.next;
+    return dummy.next;
     }
 }
