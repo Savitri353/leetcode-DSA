d003a9ce38b683cfe194aced598d9db08c9b6555
Savitri353 <savitrigamit123456@gmail.com>
2026-05-04 21:50:16 +0530

    Time: 1 ms (99.96%) | Memory: 42.2 MB (95.30%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+    
+        int rev = 0;
+        int temp=x;
+
+        while(temp != 0) {
+            int remai = temp%10;
+            if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10) {
+                return 0;
+            }
+            rev = (rev*10)+remai;
+            temp = temp/10;
+        }
+
+        return rev;
+    }
+}
