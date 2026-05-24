52c45fa62db63fd6fbcc2975879231d1114fb48f
Savitri353 <savitrigamit123456@gmail.com>
2026-05-24 09:12:50 +0530

    Time: 17 ms (17.40%) | Memory: 46.8 MB (8.83%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,26 +1,56 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+     //1. this is only for lowercase english later
+        // if(s.length() != t.length()) {
+        //     return false;
+        // }
+
+        // int[] freq = new int[26];
+
+        // for(int i=0; i<s.length(); i++) {
+        //     freq[s.charAt(i)-'a']++;
+        // }
+
+        //  for(int i=0; i<t.length(); i++) {
+        //     freq[t.charAt(i)-'a']--;
+        // }
+
+        //  for(int i=0; i<freq.length; i++) {
+        //    if(freq[i] != 0) {
+        //     return false;
+        //    } 
+        // }
+
+        // return true;
+
+        //2. this is for unicodes
+
         if(s.length() != t.length()) {
-            return false;
+             return false;
         }
 
-        int[] freq = new int[26];
+        HashMap<Character, Integer> mp = new HashMap<>();
 
+        //store frequency of s's characters
         for(int i=0; i<s.length(); i++) {
-            freq[s.charAt(i)-'a']++;
+            char c = s.charAt(i);
+            mp.put(c, mp.getOrDefault(c, 0)+1);
+        }  
+
+         //decrease frequency by using t's characters
+        for(int i=0; i<t.length(); i++) {
+            char c = t.charAt(i);
+            if(!mp.containsKey(c)) {
+                return false;
+            }
+            mp.put(c, mp.get(c)-1);
+            if(mp.get(c) == 0) {
+                mp.remove(c);
+            }
         }
 
-         for(int i=0; i<t.length(); i++) {
-            freq[t.charAt(i)-'a']--;
-        }
+        return mp.isEmpty();
 
-         for(int i=0; i<freq.length; i++) {
-           if(freq[i] != 0) {
-            return false;
-           } 
-        }
-
-        return true;
     }
 }
 
