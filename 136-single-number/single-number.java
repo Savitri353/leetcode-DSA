abadab7b90f3d4c139116da9a67897d4a904009a
Savitri353 <savitrigamit123456@gmail.com>
2026-05-31 16:43:15 +0530

    Time: 14 ms (13.49%) | Memory: 47 MB (44.87%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,17 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> fremp = new HashMap<>();
 
-        for(int i=0; i<nums.length; i++) {
-            fremp.put(nums[i], fremp.getOrDefault(nums[i], 0)+1);
-        }
+       HashMap<Integer, Integer> mp = new HashMap<>();
 
-        for(int key:fremp.keySet()) {
-            if(fremp.get(key) == 1) {
+       for(int num:nums) {
+            mp.put(num, mp.getOrDefault(num,0)+1);
+       }
+
+       for(int key:mp.keySet()) {
+            if(mp.get(key) == 1) {
                 return key;
             }
-        }
+       }
 
-        return -1;
+       return -1;
     }
 }
