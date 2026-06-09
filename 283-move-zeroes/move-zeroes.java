fa7040b61de9d75e71a793dae68d227fe403d347
Savitri353 <savitrigamit123456@gmail.com>
2026-06-09 20:46:07 +0530

    Time: 2 ms (91.78%) | Memory: 47.9 MB (19.11%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,15 +3,23 @@ class Solution {
 
         int k=0;
 
+        // for(int i=0; i<nums.length; i++) {
+        //     if(nums[i] != 0) {
+        //         int temp = nums[i];
+        //         nums[i] = nums[k];
+        //         nums[k] = temp;
+        //         k++;
+        //     }
+        // }   
+
         for(int i=0; i<nums.length; i++) {
             if(nums[i] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[k];
-                nums[k] = temp;
-                k++;
+                nums[k++] = nums[i];
             }
         }
 
-       
+        for(int i=k; i<nums.length; i++) {
+            nums[i] = 0;
+        }
     }
 }       
