749666f8baeee9fdd36d7f77b6aaa4320139c45f
Savitri353 <savitrigamit123456@gmail.com>
2026-06-09 21:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (40.70%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,27 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-        //instead of comparing with previous element i am gonna use frequancy count for each element in arr. counting sort O(n+k). with extra space
-        
-       // before counting frequancy i need to know the size of count that is going to be max element from array
 
-       int max = nums[0];
-       for(int i=1; i<nums.length; i++) {
-            max = Math.max(max, nums[i]);
-       }
+        //first calculate the frequency of each color
+        int zero=0, one=0, two=0;
+        for(int num:nums) {
 
-       int[] count = new int[max+1]; //frequancy array
-       for(int i=0; i<nums.length; i++) {
-            count[nums[i]]++;
-       }
+            if(num == 0) zero++;
+            if(num == 1) one++;
+            if(num == 2) two++;
+        }
 
-       //now rebuild arr, sorting
-       int index = 0;
-       for(int i=0; i<count.length; i++) {
-            while(count[i]>0) {
-                nums[index++] = i;
-                count[i]--;
-            }
-       }
+        int i=0;
+        while(zero != 0) {
+            nums[i++] = 0;
+            zero--;
+        }
 
+        while(one != 0) {
+            nums[i++] = 1;
+            one--;
+        }
+
+        while(two != 0) {
+            nums[i++] = 2;
+            two--;
+        }
     }
-}
+}        
