650dabbcdd0816612d09eb1f80538e968becd9cd
Savitri353 <savitrigamit123456@gmail.com>
2026-06-09 18:13:14 +0530

    Time: 7 ms (43.05%) | Memory: 55.5 MB (58.81%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,25 +1,10 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate = 0;
-        int count = 0;
-        
-        for(int i=0; i<nums.length; i++) {
-            if(count == 0) {
-                candidate = nums[i];
-            }
 
-            if(candidate == nums[i]) {
-                count++;
-            } else {
-                count--;
-            }
+        int n = nums.length;
 
-            
-        }
+         Arrays.sort(nums);
 
-        return candidate;
+         return nums[n/2];
     }
-
-}
-
-// I used Boyer–Moore Voting Algorithm which works in O(n) time and O(1) space. Since the majority element appears more than n/2 times, it cannot be eliminated by pairwise cancellation.
+}      
