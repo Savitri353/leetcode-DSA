0fff074b4ee2b5dfe6a7d6387841554f6abe906a
Savitri353 <savitrigamit123456@gmail.com>
2026-06-19 15:56:21 +0530

    Time: 1 ms (87.77%) | Memory: 45.6 MB (29.74%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -3,32 +3,29 @@ import java.util.*;
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
-
         boolean[] visited = new boolean[nums.length];
 
-        solve(list, new ArrayList<>(), visited, nums);
+        solve(list, visited, new ArrayList<>(), nums);
         return list;
-    } 
+    }
 
-    public void solve(List<List<Integer>> list, List<Integer> temp, boolean[] visited, int[] nums) {
-        if(temp.size() == nums.length) {
+    public void solve(List<List<Integer>> list, boolean[] visited, List<Integer> temp, int[] nums) {
+
+        if(temp.size()==nums.length) {
             list.add(new ArrayList<>(temp));
             return;
         }
 
         for(int i=0; i<nums.length; i++) {
-            if(visited[i]) continue;
-
-            temp.add(nums[i]); //add
+            if(visited[i]) {
+                continue;
+            }
             visited[i] = true;
-            solve(list, temp, visited, nums); //explore
-            
+            temp.add(nums[i]); //do
+            solve(list, visited,temp, nums); //explore
             temp.remove(temp.size()-1); //undo
             visited[i] = false;
-            //explore i=i+1
         }
     }
+}       
 
-}
-
-//O(n × n!) time complexity
