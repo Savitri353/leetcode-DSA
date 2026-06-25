b56fe231d17103ab57683bf100de31bdac128aba
Savitri353 <savitrigamit123456@gmail.com>
2026-06-25 18:51:53 +0530

    Time: 17 ms (16.79%) | Memory: 46.7 MB (15.84%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,25 +1,54 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
+        // if(s.length() != t.length()) return false;
+
+        // int n = s.length();
+        // int[] freq = new int[26];
+
+        // for(int i=0; i<n; i++) {
+        //     freq[s.charAt(i) - 'a']++;
+        // }
+
+        // for(int i=0; i<n; i++) {
+        //     freq[t.charAt(i) - 'a']--;
+        // }
+
+        //  for(int i=0; i<26; i++) {
+        //     if(freq[i] != 0) {
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
         if(s.length() != t.length()) return false;
 
-        int n = s.length();
-        int[] freq = new int[26];
+        //to store frequency
+        HashMap<Character, Integer> mp = new HashMap<>();
 
-        for(int i=0; i<n; i++) {
-            freq[s.charAt(i) - 'a']++;
+
+        //first store the frequency of first string's character
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            mp.put(c, mp.getOrDefault(c, 0)+1);
         }
 
-        for(int i=0; i<n; i++) {
-            freq[t.charAt(i) - 'a']--;
-        }
+        //now reduce the frequency of characters
+         for(int i=0; i<t.length(); i++) {
+            char c = t.charAt(i);
 
-         for(int i=0; i<26; i++) {
-            if(freq[i] != 0) {
+            if(!mp.containsKey(c)) {
                 return false;
+            }
+
+            mp.put(c, mp.get(c)-1);
+
+            if(mp.get(c) == 0) {
+                mp.remove(c);
             }
         }
 
-        return true;
+        return mp.isEmpty();
     }
 }     
