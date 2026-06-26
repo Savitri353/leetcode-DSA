28e40baef2ca77c6426c861999353637312d6c7e
Savitri353 <savitrigamit123456@gmail.com>
2026-06-26 21:28:08 +0530

    Time: 3 ms (87.33%) | Memory: 43.6 MB (15.43%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
 
-        if(s.length()<2) {
+        if(s.length() % 2 != 0) {
             return false;
         }
 
@@ -10,13 +10,14 @@ class Solution {
         for(int i=0; i<s.length(); i++) {
             char c = s.charAt(i);
 
-            if(c == '(' || c == '{' || c == '[') {
+            if(c == '(' || c == '{' || c == '[' ) {
                 st.push(c);
+
             } else if(c == ')' && !st.isEmpty() && st.peek() == '(') {
                 st.pop();
-            } else if(c == '}' && !st.isEmpty() && st.peek() == '{') {
+            }else if(c == '}' && !st.isEmpty() && st.peek() == '{') {
                 st.pop();
-            } else if(c == ']' && !st.isEmpty() && st.peek() == '[') {
+            }else if(c == ']' && !st.isEmpty() && st.peek() == '[') {
                 st.pop();
             } else {
                 return false;
