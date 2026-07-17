d1df7e65e0a00a5656fda82074845d54e6c5a344
Savitri353 <savitrigamit123456@gmail.com>
2026-07-17 09:38:43 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (25.16%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,58 +1,33 @@
 class Solution {
     public void sortColors(int[] nums) {
+        int countZero = 0;
+        int countOne = 0;
+        int countTwo = 0;
 
-        // approach 1
-        //first calculate the frequency of each color
-        int zero=0, one=0, two=0;
-        for(int num:nums) {
-
-            if(num == 0) zero++;
-            if(num == 1) one++;
-            if(num == 2) two++;
+        for(int n:nums) {
+            if(n == 0) {
+                countZero++;
+            } else if(n == 1) {
+                countOne++;
+            } else {
+                countTwo++;
+            }   
         }
 
-        //then build new nums in-place according to frequency
         int i=0;
-        while(zero != 0) {
+        while(countZero>0) {
             nums[i++] = 0;
-            zero--;
+            countZero--;
         }
 
-        while(one != 0) {
+        while(countOne>0) {
             nums[i++] = 1;
-            one--;
+            countOne--;
         }
 
-        while(two != 0) {
+        while(countTwo>0) {
             nums[i++] = 2;
-            two--;
+            countTwo--;
         }
-
-        //approach 2
-
-        // int mid = 0;
-        // int low = 0;
-        // int high = nums.length-1;
-
-        // while(mid <= high) {
-
-        //     if(nums[mid] == 1) {
-
-        //         mid++;
-        //     } else if(nums[mid] == 0) {
-
-        //         int temp = nums[mid];
-        //         nums[mid] = nums[low];
-        //         nums[low] = temp;
-        //         low++;
-        //         mid++;
-
-        //     } else {
-        //         int temp = nums[mid];
-        //         nums[mid] = nums[high];
-        //         nums[high] = temp;
-        //         high--;
-        //     }
-        // }
     }
-}        
+}    
