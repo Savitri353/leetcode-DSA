a80d4532aa1488d6542f1a6ecdc394c4a5704b59
Savitri353 <savitrigamit123456@gmail.com>
2026-07-17 17:39:14 +0530

    Time: 388 ms (62.35%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,2 +1,4 @@
-select e1.name as Employee from Employee as e1 join Employee as e2
-on e1.managerId = e2.id where e1.salary>e2.salary;
+
+select e1.name as employee from employee e1 join employee e2 
+on e1.managerId  = e2.id
+where e1.salary > e2.salary;
