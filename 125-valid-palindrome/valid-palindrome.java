ab6bc746c25e604005d5c7742fcb8bc0811e2f54
Savitri353 <savitrigamit123456@gmail.com>
2026-07-25 19:27:07 +0530

    Time: 2 ms (99.29%) | Memory: 44.1 MB (92.05%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,9 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-      int i=0, j=s.length()-1;
-
-       while(i<=j) {
         
+        // s = s.trim();
+
+        int i=0, j=s.length()-1;
+
+        while(i<=j) {
+
             while(i<=j && !Character.isLetterOrDigit(s.charAt(i))) i++;
             while(i<=j && !Character.isLetterOrDigit(s.charAt(j))) j--;
 
@@ -13,9 +16,8 @@ class Solution {
 
             i++;
             j--;
-       }
+        }
 
-    return true;
-}
-
+        return true;
+    }
 }
