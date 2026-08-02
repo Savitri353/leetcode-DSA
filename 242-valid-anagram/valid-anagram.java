6f5934f49b0087e390376e6043ed6ebdfec75f1f
Savitri353 <savitrigamit123456@gmail.com>
2026-08-02 12:00:24 +0530

    Time: 17 ms (16.53%) | Memory: 46.8 MB (9.36%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,43 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        // if(s.length() != t.length()) return false;
-
-        // int n = s.length();
-        // int[] freq = new int[26];
-
-        // for(int i=0; i<n; i++) {
-        //     freq[s.charAt(i) - 'a']++;
-        // }
-
-        // for(int i=0; i<n; i++) {
-        //     freq[t.charAt(i) - 'a']--;
-        // }
-
-        //  for(int i=0; i<26; i++) {
-        //     if(freq[i] != 0) {
-        //         return false;
-        //     }
-        // }
-
-        // return true;
-
-        if(s.length() != t.length()) return false;
-
-        //to store frequency
+        
         HashMap<Character, Integer> mp = new HashMap<>();
 
-
-        //first store the frequency of first string's character
         for(int i=0; i<s.length(); i++) {
             char c = s.charAt(i);
-            mp.put(c, mp.getOrDefault(c, 0)+1);
+            mp.put(c, mp.getOrDefault(c,0)+1);
         }
 
-        //now reduce the frequency of characters
-         for(int i=0; i<t.length(); i++) {
+        for(int i=0; i<t.length(); i++) {
             char c = t.charAt(i);
-
+            
             if(!mp.containsKey(c)) {
                 return false;
             }
@@ -51,4 +24,4 @@ class Solution {
 
         return mp.isEmpty();
     }
-}     
+}
