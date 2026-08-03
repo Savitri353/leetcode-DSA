1d394e141662a2a730a622ccf7565ecc1ebf1223
Savitri353 <savitrigamit123456@gmail.com>
2026-08-03 18:43:42 +0530

    Time: 4 ms (36.80%) | Memory: 43.7 MB (7.46%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-
+        
         Stack<Character> st = new Stack<>();
 
         for(int i=0; i<s.length(); i++) {
+
             char c = s.charAt(i);
 
-            if(c == '(' || c == '{' || c == '[') {
+            if(c == '('||c == '['||c == '{') {
                 st.push(c);
-            } else if (c == ')' && !st.isEmpty() && st.peek() == '(') {
+            } else if(c==')' && !st.isEmpty() && st.peek() == '(') {
                 st.pop();
-            } else if (c == '}' && !st.isEmpty() && st.peek() == '{') {
+            } else if(c=='}' && !st.isEmpty() && st.peek() == '{') {
                 st.pop();
-            } else if (c == ']' && !st.isEmpty() && st.peek() == '[') {
+            } else if(c==']' && !st.isEmpty() && st.peek() == '[') {
                 st.pop();
             } else {
                 return false;
@@ -21,4 +22,4 @@ class Solution {
 
         return st.isEmpty();
     }
-}    
+}
