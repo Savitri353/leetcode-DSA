c048772d3df460c30862adf542d34a77260f91f1
Savitri353 <savitrigamit123456@gmail.com>
2026-08-04 21:54:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (33.79%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,8 +1,23 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();
-        String[] arr = s.split("\\s+");
 
-        return arr[arr.length-1].length();
+        //aproachh 1
+        // s = s.trim();
+        // String[] arr = s.split("\\s+");
+
+        // return arr[arr.length-1].length();
+
+        //approach 2
+
+        s = s.trim();
+        int i=s.length()-1;
+
+        int l=0;
+        while(i>=0 && s.charAt(i) != ' ') {
+            l++;
+            i--;
+        }
+
+        return l;
     }
 }
