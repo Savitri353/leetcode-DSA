ee0800af0762cca2328ecf339483a79b98724b14
Savitri353 <savitrigamit123456@gmail.com>
2026-08-06 21:56:38 +0530

    Time: 2 ms (99.58%) | Memory: 49.5 MB (16.89%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        
+        // Stack<Integer> st = new Stack<>();
+
+        int[] ans = new int[arr.length];
+        int max = -1;
+
+        for(int i=arr.length-1; i>=0; i--) {
+            ans[i] = max;
+            max = Math.max(max, arr[i]);
+        }
+
+        return ans;
+    }
+}
