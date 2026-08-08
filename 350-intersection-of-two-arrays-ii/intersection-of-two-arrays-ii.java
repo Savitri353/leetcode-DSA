8cbf65421ab5c8713c71353f539ca4a0fa01ff4b
Savitri353 <savitrigamit123456@gmail.com>
2026-08-08 11:40:45 +0530

    Time: 3 ms (95.32%) | Memory: 45 MB (66.89%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,30 +1,25 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> mp = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
+        
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        ArrayList<Integer> list  = new ArrayList<>();
 
-        for(int i=0; i<nums1.length; i++) {
-            if(mp.containsKey(nums1[i])) {
-                mp.put(nums1[i], mp.get(nums1[i])+1);
-            } else {
-                mp.put(nums1[i], 1);
+        for(int num:nums2) {
+            mp.put(num, mp.getOrDefault(num, 0)+1);
+        }  
+
+        for(int num:nums1) {
+            if(mp.getOrDefault(num, 0)>0) {
+               list.add(num);
+               mp.put(num, mp.get(num)-1);
             }
         }
 
-        for(int i=0; i<nums2.length; i++) {
-            if(mp.containsKey(nums2[i]) && mp.get(nums2[i]) >= 1) {
-                list.add(nums2[i]);
-                mp.put(nums2[i], mp.get(nums2[i])-1);
-            } 
-        }
-
-        int[] arr = new int[list.size()];
+        int[] ans = new int[list.size()];
         int i=0;
-        for(int l:list) {
-            arr[i++] = l;
+        for(int e:list) {
+            ans[i++] = e;
         }
-
-        return arr;
+        return ans;
     }
 }
-//I use a frequency map for the first array and then iterate over the second array, adding elements to the result list while decreasing their count
