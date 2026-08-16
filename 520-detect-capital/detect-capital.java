39862ff52846365dd39dccb5fc8d2fa013352817
Savitri353 <savitrigamit123456@gmail.com>
2026-08-16 21:35:36 +0530

    Time: 1 ms (89.46%) | Memory: 43.5 MB (11.96%) - LeetSync

--- 520-detect-capital/detect-capital.java
+++ 520-detect-capital/detect-capital.java
@@ -1,33 +1,27 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
         
-        //if first character is uppercase then there will be two case, either only first or whole string will be into uppercase
-       if(word.length() == 1) return true;
-        if(Character.isUpperCase(word.charAt(0))) { 
-            if(Character.isUpperCase(word.charAt(1))) {
+        int count = 0;
 
-                 for(int i=2; i<word.length(); i++) {
-                    if(!Character.isUpperCase(word.charAt(i))) {
-                        return false;
-                    }
-                 }
+        for(int i=0; i<word.length(); i++) {
 
-            } else {
-
-                for(int i=1; i<word.length(); i++) {
-                    if(Character.isUpperCase(word.charAt(i))) {
-                        return false;
-                    }
-                 }
-            }      
-        } else {
-            for(int i=0; i<word.length(); i++) {
-                    if(Character.isUpperCase(word.charAt(i))) {
-                        return false;
-                    }
+            if(Character.isUpperCase(word.charAt(i))) {
+                count++;
             }
-        }   
+        }
 
-        return true;    
+        if(count == word.length()) {
+            return true;
+        }
+
+        if(count == 0) {
+            return true;
+        }
+
+        if(count == 1 && Character.isUpperCase(word.charAt(0))){
+            return true;
+        }
+
+        return false;
     }
 }
