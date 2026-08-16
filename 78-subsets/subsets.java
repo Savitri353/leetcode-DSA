b0bf5c21250ea4983edf928b5bb127be693a5b17
Savitri353 <savitrigamit123456@gmail.com>
2026-08-16 21:16:38 +0530

    Time: 1 ms (86.44%) | Memory: 44.3 MB (39.35%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,23 +1,22 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-    List<List<Integer>> list = new ArrayList<>();
-    
-       solve(0, nums, new ArrayList<>(), list);
-       return list;
+        List<List<Integer>> list = new ArrayList<>();
+        solve(0, nums,new ArrayList<>(), list);
+        return list;
     }
 
-   public void solve(int i, int[] nums, List<Integer> temp, List<List<Integer>> list) {
-        if(i == nums.length) {
+    public void solve(int i, int[] nums, List<Integer> temp, List<List<Integer>> list) {
+        if(i==nums.length) {
             list.add(new ArrayList<>(temp));
             return;
         }
 
+        
         temp.add(nums[i]); //take
-        solve(i+1, nums, temp, list); //explore
+        solve(i+1, nums, temp, list); //Explore
         temp.remove(temp.size()-1); //undo
         solve(i+1, nums, temp, list); //explore
     }
-
 }
 
 //tc : every element has two options(take or not take) and we have two elements: so 2^n
