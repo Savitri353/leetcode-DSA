1659182a661b5345c3942c78893bb03646c6c58f
Savitri353 <savitrigamit123456@gmail.com>
2026-08-17 15:29:44 +0530

    Time: 23 ms (93.73%) | Memory: 48.5 MB (79.67%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,23 +1,22 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-       HashMap<Integer, Integer> mp = new HashMap<>();
+        
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
         mp.put(0,1);
-       int currentSum = 0;
-       int result=0;
+        int cumSum = 0;
+        int count =0;
 
-       for(int i=0; i<nums.length; i++) {
-        currentSum+=nums[i];
+        for(int i=0; i<nums.length; i++) {
+            cumSum+=nums[i];
 
-        if(mp.containsKey(currentSum-k)) {
-            result += mp.get(currentSum-k);
-        } 
-        mp.put(currentSum, mp.getOrDefault(currentSum, 0)+1);
-       }
+            if(mp.containsKey(cumSum-k)) {
+                count+=mp.get(cumSum-k);
+            }
 
-       return result;
+            mp.put(cumSum, mp.getOrDefault(cumSum,0)+1);
+        }
+
+        return count;
     }
 }
-//O(n) tc
-//1️ Keep a running prefix sum and a HashMap to store its frequency.
-//2 For each element, if (currentSum − k) exists in the map, add its count to the answer.
-//3 Update the map with the current prefix sum.
